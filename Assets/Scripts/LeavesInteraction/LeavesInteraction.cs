659ef2a5a1c69b1ab23ace4ab7f42f814104b9f5
Jordi Caballol <jcaballol94@gmail.com>
2022-05-05 15:33:19 +0100

    Small test for the interaction

---
+++ Assets/Scripts/LeavesInteraction/LeavesInteraction.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.VFX;
+
+namespace jCaballol94.Leaves
+{
+    public class LeavesInteraction : MonoBehaviour
+    {
+        private readonly int SPHERE = Shader.PropertyToID("InteractionSphere");
+        private readonly int VELOCITY = Shader.PropertyToID("InteractionSpeed");
+
+        public VisualEffect effect;
+
+        public float Radius => Mathf.Max(transform.lossyScale.x, transform.lossyScale.y, transform.lossyScale.z);
+
+        private Vector3 m_previousPos;
+
+        private void Start()
+        {
+            m_previousPos = transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            var dist = transform.position - m_previousPos;
+            var velocity = dist / Time.deltaTime;
+
+            effect.SetVector4(SPHERE, new Vector4(transform.position.x, transform.position.y, transform.position.z, Radius));
+            effect.SetVector3(VELOCITY, velocity);
+
+            m_previousPos = transform.position;
+        }
+
+        private void OnDrawGizmosSelected()
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(transform.position, Radius);
+        }
+    }
+}
